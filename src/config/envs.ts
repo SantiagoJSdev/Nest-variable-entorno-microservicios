a5f550d822287fb523904aa4fc81f25c21b0a342
Santiago <santiagojose17@gmail.com>
2024-04-06 17:19:17 -0400

    first commit

---
+++ src/config/envs.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+//aca valide que el puerto sea un numero
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = joi.object({
+  PORT: joi.number().required()
+})
+.unknown(true);
+
+const { error, value } = envsSchema.validate( process.env );
+
+
+if ( error ) {
+  throw new Error(`Config validation error: ${ error.message }`);
+}
+
+const envVars:EnvVars = value;
+ 
+
+export const envs = {
+ port: envVars.PORT,
+ // port: process.env.PORT,
+}
